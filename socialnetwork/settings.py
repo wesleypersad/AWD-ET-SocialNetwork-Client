8bb4637485410281177a828963a12192c919ad67
Wesley Persad <26929215+wesleypersad@users.noreply.github.com>
2023-06-12 15:37:10 +0100

    Setting modified for trusted origins

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -160,3 +160,31 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+# Add the commands to set trusted origins
+CORS_ORIGIN_WHITELIST = (
+    '*',
+)
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
