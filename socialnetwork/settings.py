aff028eaf60c92eab6a630890f34818420b2f729
Wesley Persad <26929215+wesleypersad@users.noreply.github.com>
2023-08-25 15:58:00 +0100

    Debug flag set to fix iamges issue

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -30,7 +30,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# Images on profiles may disappear whne this is False ?
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "*"          
