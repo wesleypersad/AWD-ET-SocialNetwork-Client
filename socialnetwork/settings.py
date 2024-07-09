042a5eabf325ef39f2b4e659483aa062c4adb503
Wesley Persad <vasudeo.persad@gmail.com>
2024-07-09 16:03:40 +0100

    Fixed warnings in migrations

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -140,10 +140,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '/userdata/static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
--- userdata/apps.py
+++ userdata/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 
 class UserdataConfig(AppConfig):
     name = 'userdata'
+    default_auto_field = 'django.db.models.BigAutoField'
 
     def ready(self):
         import userdata.signals
