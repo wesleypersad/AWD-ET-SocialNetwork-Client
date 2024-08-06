1b561401dead521cfab1cdc85eec74f919482ec4
Wesley Persad <vasudeo.persad@gmail.com>
2024-08-06 11:59:23 +0100

    Graphics display bug fixed

--- socialnetwork/urls.py
+++ socialnetwork/urls.py
@@ -18,6 +18,18 @@ from django.urls import include, path
 # requires the follwing for the API displays
 from rest_framework.schemas import get_schema_view
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # Your existing URL patterns
+    ...
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = [
     path('', include('userdata.urls')),
@@ -34,3 +46,8 @@ urlpatterns = [
         extra_context={'schema_url': 'openapi-schema'}
     ), name='swagger-ui')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
